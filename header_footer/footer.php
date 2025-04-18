313d86378e99cd768018222316cad0f2773ea262
yoonmi123 <160564902+yoonmi123@users.noreply.github.com>
2025-04-18 14:27:52 +0630

    Add files via upload

---
+++ header_footer/footer.php
@@ -0,0 +1,163 @@
+<?php 
+$isMember = $isMember??'';
+$isAdmin = $isAdmin??'';
+
+
+$isAdminMemberFromPJwebpage = $isAdminMemberFromPJwebpage??'';
+// echo $isAdminMemberFromPJwebpage;
+
+?>
+<!Doctype html>
+<head>
+    <title>footer</title>
+      <!-- font awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <!-- custom css  -->
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+
+</head>
+<body>
+
+  
+
+    <div class="Yfooter-container ">
+  <!-- Footer -->
+  <footer class="text-center text-lg-start">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="YFooterLinkSection">
+        <!--Grid row-->
+        <div class="row">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold"> What is KanBan Board? </h6>
+            <p>
+            A kanban board is a physical or digital project management tool designed to help visualize work, limit work-in-progress, and maximize efficiency(or flow).
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+            <p>
+              <a href="#">EPDAPHP</a>
+            </p>
+            <p>
+              <a href="#">EPDAHTML</a>
+            </p>
+            <p>
+              <a href="#">EPDACSS</a>
+            </p>
+            <p>
+              <a href="#">EPDA JAVASCRIPT</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+              Useful links
+            </h6>
+            <p>
+              <a href="#">Your Account</a>
+            </p>
+            <p>
+              <a href="#">Member List</a>
+            </p>
+            <p>
+              <a href="#">See your Projects</a>
+            </p>
+            <p>
+              <a href="#">Help</a>
+            </p>
+          </div>
+
+          <!-- Grid column -->
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p><i class="fas fa-home mr-3"></i> 10th Floor,YZN Tower</p>
+            <p><i class="fas fa-envelope mr-3"></i> epda@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> + 09 740 939 724</p>
+            <p><i class="fas fa-print mr-3"></i> + 09 740 939 724</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr class="my-3">
+
+      <!-- Section: Copyright -->
+      <section class="p-3 pt-0 YFooterLinkSection">
+        <div class="row d-flex align-items-center">
+          <!-- Grid column -->
+          <div class="col-md-7 col-lg-8 text-center text-md-start">
+            <!-- Copyright -->
+            <div class="p-3">
+              © 2024 Copyright:
+              <a href="#">EPDAKanbanBoard.com</a>
+            </div>
+            <!-- Copyright -->
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end YFooterIcon">
+            <!-- Facebook -->
+            <a class="btn btn-outline-light btn-floating m-1" role="button">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+
+            <!-- Twitter -->
+            <a class="btn btn-outline-light btn-floating m-1 " role="button">
+               <i class="fab fa-twitter"></i >
+            </a>
+
+            <!-- Google -->
+            <a class="btn btn-outline-light btn-floating m-1" role="button">
+              <i class="fab fa-google"></i>
+            </a>
+
+            <!-- Instagram -->
+           
+              <a class="btn btn-outline-light btn-floating m-1 " role="button">
+                <i class="fab fa-instagram"></i>
+             </a>
+
+              
+          </div>
+          <!-- Grid column -->
+        </div>
+      </section>
+      <!-- Section: Copyright -->
+    </div>
+    <!-- Grid container -->
+  </footer>
+  <!-- Footer -->
+</div>
+<!-- End of .container -->
+
+
+
+
+
+  <!--bootstrap css1 js 1-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
